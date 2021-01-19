216899d67e7a416cba0f8d3f827720b8b4966bc2
Jodi <jodi.kettle@gmail.com>
2021-01-19 16:35:53 +0000

    More scripting

--- devops/CreateDeploymentSlot.ps1
+++ devops/CreateDeploymentSlot.ps1
@@ -1,20 +1,22 @@
-Write-Host "Powershell starting"
+Write-Host "Powershell start"
 
 $resourceGroupName = $OctopusParameters["ResourceGroupName"]
 Write-Host "Resource group name $resourceGroupName"
 
 $appName = $OctopusParameters["AppName"]
 
+$slotName = $OctopusParameters["SlotName"]
+
 Write-Host "Application name $appName"
 
 Try 
 {
-	Get-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $appName -Slot batonbot
+	Get-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $appName -Slot $slotName
 	Write-Host "Environment exists." -ForegroundColor blue
 }
 Catch 
 {
 	Write-Host "Environment does not exist. Create new environment" -ForegroundColor red
 	Write-Host "Create new environment" -ForegroundColor green
-	New-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $appName -Slot batonbot
+	New-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $appName -Slot $slotName
 }
