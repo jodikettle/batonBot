cc820d611c483ce11233477cd798e76674cbf490
Jodi <jodi.kettle@gmail.com>
2021-01-19 14:50:37 +0000

    Add the deployment slot

---
+++ devops/CreateDeploymentSlot.ps1
@@ -0,0 +1,18 @@
+$resourceGroupName = $OctopusParameters["ResourceGroupName"]
+Write-Host "Resource group name $resourceGroupName"
+
+$appName = $OctopusParameters["AppName"]
+
+Write-Host "Application name $appName"
+
+Try 
+{
+	Get-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $appName -Slot batonbot
+	Write-Host "Environment exists." -ForegroundColor blue
+}
+Catch 
+{
+	Write-Host "Environment does not exist. Create new environment" -ForegroundColor red
+	Write-Host "Create new environment" -ForegroundColor green
+	New-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $appName -Slot batonbot
+}
